797198096cf803bfb58aca50bf0905899950241c
stevewarui,@gmail.com <stevewarum@gmail.com>
2023-09-14 18:56:43 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names=["Guadalupe", "Ollie", "Aki"];
+const events ='surprise';
+
+  function writeCards(){
+    let newarray=[];
+    for(let i=0; i < names.length; i++){
+        newarray.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+    }
+    return newarray;
+  }
+  console.log(writeCards(names,events));
+
+  function countDown(positiveNumber){
+    while (positiveNumber >=0){
+        console.log(positiveNumber)
+        positiveNumber--;
+    }
+  }
+  console.log(countDown(10));
